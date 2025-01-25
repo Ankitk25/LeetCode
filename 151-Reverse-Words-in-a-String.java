330b7f8b3974a4b693ee8e6ede210457a1fa6c45
Ankitk25 <157107620+Ankitk25@users.noreply.github.com>
2025-01-25 16:12:36 +0530

    [151] [Time Beats: 93.44%] [Memory Beats: 36.57%] - LeetPush

---
+++ 151-Reverse-Words-in-a-String.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] str= s.split(" ");
+        StringBuilder res= new StringBuilder();
+        
+        for (int i = str.length-1; i >= 0; i--) {
+            if (str[i].length()==0){
+                continue;
+            }
+            if (res.length()==0){
+                res.append(str[i]);
+            }
+            else {
+                res.append(" ");
+                res.append(str[i]);
+            }
+        }
+        return res.toString();
+        
+    }
+}
